896d049d18883e864d35d8efa5ab93e67579283a
JimmyNkosi <jimmynkosi2000@gmail.com>
2023-08-16 15:35:36 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
-- permises SQL
--- show 1 and 2 users
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
